69dbe8b330e16877f3e96dc6b6352be10d3c1008
moemode <pfeffer.moritz@gmail.com>
2024-07-17 18:29:49 +0200

    Solve a2 RisingTides

---
+++ Assignment 2/code/RisingTides.cpp
@@ -0,0 +1,307 @@
+#include "RisingTides.h"
+#include "GUI/SimpleTest.h"
+#include "queue.h"
+#include <queue>
+
+using namespace std;
+
+std::array<GridLocation, 4> neighbors(const GridLocation& location) {
+    std::array<GridLocation, 4> neighborLocations = {
+        GridLocation(location.row - 1, location.col), // Up
+        GridLocation(location.row + 1, location.col), // Down
+        GridLocation(location.row, location.col - 1), // Left
+        GridLocation(location.row, location.col + 1)  // Right
+    };
+    return neighborLocations;
+}
+
+Grid<bool> floodedRegionsIn(const Grid<double>& terrain,
+                            const Vector<GridLocation>& sources,
+                            double height) {
+    Grid<bool> isFlooded(terrain.numRows(), terrain.numCols());
+    isFlooded.fill(false);
+    queue<GridLocation> q;
+    for(const GridLocation &location: sources) {
+        if(terrain[location] <= height) {
+            isFlooded[location] = true;
+            q.push(location);
+        }
+    }
+    for(; !q.empty(); q.pop()) {
+        for(const GridLocation& neighbor: neighbors(q.front())) {
+            if(terrain.inBounds(neighbor) && !isFlooded[neighbor] && terrain[neighbor] <= height) {
+                isFlooded[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+    return isFlooded;
+}
+
+
+/***** Test Cases Below This Point *****/
+PROVIDED_TEST("Nothing gets wet if there are no water sources.") {
+    Grid<double> world = {
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 }
+    };
+
+    Vector<GridLocation> sources = {
+        // empty
+    };
+
+    /* There are no water sources, so nothing should be underwater. */
+    Grid<bool> water = floodedRegionsIn(world, sources, 1.0);
+    Grid<bool> expected = {
+        { false, false, false },
+        { false, false, false },
+        { false, false, false }
+    };
+
+    EXPECT_EQUAL(water, expected);
+}
+
+PROVIDED_TEST("Everything gets wet if all locations are below the water level.") {
+    Grid<double> world = {
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 }
+    };
+
+    Vector<GridLocation> sources = {
+        { 0, 0 }
+    };
+
+    /* Everything should flood; there are no barriers to stop the water. */
+    Grid<bool> water = floodedRegionsIn(world, sources, 1.0);
+    Grid<bool> expected = {
+        {  true,  true,  true },
+        {  true,  true,  true },
+        {  true,  true,  true }
+    };
+
+    EXPECT_EQUAL(water, expected);
+}
+
+PROVIDED_TEST("Water can't cross a vertical levee.") {
+    Grid<double> world = {
+        { 0, 2, 0 },
+        { 0, 2, 0 },
+        { 0, 2, 0 }
+    };
+
+    Vector<GridLocation> sources = {
+        { 0, 0 }
+    };
+
+    /* Only locations to the left of the barrier should be under water. */
+    Grid<bool> water = floodedRegionsIn(world, sources, 1.0);
+    Grid<bool> expected = {
+        {  true, false, false },
+        {  true, false, false },
+        {  true, false, false }
+    };
+
+    EXPECT_EQUAL(water, expected);
+}
+
+PROVIDED_TEST("Water can't cross a diagonal levee.") {
+    Grid<double> world = {
+        { 0, 0, 2 },
+        { 0, 2, 0 },
+        { 2, 0, 0 }
+    };
+
+    Vector<GridLocation> sources = {
+        { 0, 0 }
+    };
+
+    /* Water only flows in the four cardinal directions, so it can't
+     * pass through the barrier. Only the top should be flooded.
+     */
+    Grid<bool> water = floodedRegionsIn(world, sources, 1.0);
+    Grid<bool> expected = {
+        {  true,  true, false },
+        {  true, false, false },
+        { false, false, false }
+    };
+
+    EXPECT_EQUAL(water, expected);
+}
+
+PROVIDED_TEST("Water can't flow diagonally.") {
+    Grid<double> world = {
+        { 0, 2, 0 },
+        { 2, 0, 2 },
+        { 0, 2, 0 }
+    };
+
+    Vector<GridLocation> sources = {
+        { 1, 1 }
+    };
+
+    /* Water should be trapped in the center, since it can't move
+     * diagonally.
+     */
+    Grid<bool> water = floodedRegionsIn(world, sources, 1.0);
+    Grid<bool> expected = {
+        { false, false, false },
+        { false,  true, false },
+        { false, false, false }
+    };
+
+    EXPECT_EQUAL(water, expected);
+}
+
+PROVIDED_TEST("Water can flow in all cardinal directions.") {
+    Grid<double> world = {
+        { 2, 0, 2 },
+        { 0, 0, 0 },
+        { 2, 0, 2 }
+    };
+
+    Vector<GridLocation> sources = {
+        { 1, 1 }
+    };
+
+    /* The water in this case should flow up, down, left, and right. */
+    Grid<bool> water = floodedRegionsIn(world, sources, 1.0);
+    Grid<bool> expected = {
+        { false,  true, false },
+        {  true,  true,  true },
+        { false,  true, false }
+    };
+
+    EXPECT_EQUAL(water, expected);
+}
+
+PROVIDED_TEST("Water can flow from multiple sources.") {
+    Grid<double> world = {
+        { 0, 0, 2 },
+        { 0, 2, 0 },
+        { 2, 0, 0 }
+    };
+
+    Vector<GridLocation> sources = {
+        { 0, 0 },
+        { 2, 2 }
+    };
+
+    /* Everything except the levee should be under water. */
+    Grid<bool> water = floodedRegionsIn(world, sources, 1.0);
+    Grid<bool> expected = {
+        {  true,  true, false },
+        {  true, false,  true },
+        { false,  true,  true }
+    };
+
+    EXPECT_EQUAL(water, expected);
+}
+
+PROVIDED_TEST("Handles asymmetric worlds and non-square grids") {
+    Grid<double> world = {
+        { 3, 1, 4, 1 },
+        { 5, 9, 2, 6 },
+        { 5, 3, 5, 8 }
+    };
+
+    /* Initial test - water shouldn't leak out from the 2 if the height is 3.5. */
+    Vector<GridLocation> sources = {
+        { 1, 2 }
+    };
+
+    Grid<bool> expected = {
+        { false, false, false, false },
+        { false, false,  true, false },
+        { false, false, false, false },
+    };
+
+    EXPECT_EQUAL(floodedRegionsIn(world, sources, 3.5), expected);
+
+    /* Now, increase the water height to 4.5. */
+    expected = {
+        {  true,  true,  true,  true },
+        { false, false,  true, false },
+        { false, false, false, false },
+    };
+
+    EXPECT_EQUAL(floodedRegionsIn(world, sources, 4.5), expected);
+
+    /* Now, increase the water height to 5.5. */
+    expected = {
+        {  true,  true,  true,  true },
+        {  true, false,  true, false },
+        {  true,  true,  true, false },
+    };
+
+    EXPECT_EQUAL(floodedRegionsIn(world, sources, 5.5), expected);
+
+    /* Now, increase the water height to 6.5. */
+    expected = {
+        {  true,  true,  true,  true },
+        {  true, false,  true,  true },
+        {  true,  true,  true, false },
+    };
+
+    EXPECT_EQUAL(floodedRegionsIn(world, sources, 6.5), expected);
+
+    /* Now, increase the water height to 9.5. */
+    expected = {
+        {  true,  true,  true,  true },
+        {  true,  true,  true,  true },
+        {  true,  true,  true,  true },
+    };
+
+    EXPECT_EQUAL(floodedRegionsIn(world, sources, 9.5), expected);
+}
+
+PROVIDED_TEST("Stress test: Handles a large, empty world quickly.") {
+    Grid<double> world(100, 100); // Large world, everything defaults to 0 height.
+    Vector<GridLocation> sources = {
+        { 0, 0 }
+    };
+
+    /* This may take a long time to complete if the solution is inefficient. Look
+     * for things like
+     *
+     * 1. passing around large objects by *value* rather than by *reference*,
+     * 2. revisiting the same squares multiple times (e.g. flooding the same
+     *    cell many times due to not checking if something is flooded),
+     *
+     * etc.
+     */
+    Grid<bool> water = floodedRegionsIn(world, sources, 1.0);
+    EXPECT_EQUAL(water.numRows(), world.numRows());
+    EXPECT_EQUAL(water.numCols(), world.numCols());
+
+    /* Everything should be flooded. */
+    for (int row = 0; row < world.numRows(); row++) {
+        for (int col = 0; col < world.numCols(); col++) {
+            EXPECT_EQUAL(water[row][col], true);
+        }
+    }
+}
+
+
+STUDENT_TEST("Source that is higher than water is not flooded.") {
+    Grid<double> world = {
+        { 4, 2, 1 },
+        { 9, 3, 4 },
+        { 1, 4, 0 }
+    };
+
+    Vector<GridLocation> sources = {
+        { 0, 0 }
+    };
+
+    /* Only locations to the left of the barrier should be under water. */
+    Grid<bool> water = floodedRegionsIn(world, sources, 3);
+    Grid<bool> expected = {
+        {  false, false, false },
+        {  false, false, false },
+        {  false, false, false }
+    };
+
+    EXPECT_EQUAL(water, expected);
+}
